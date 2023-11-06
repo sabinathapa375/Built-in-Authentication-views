9182a9c448beaa1216af65d164a68ff8250218bc
sabinathapa375 <149649417+sabinathapa375@users.noreply.github.com>
2023-11-06 20:34:55 +0545

    View updated

--- AuthenticationApp/views.py
+++ AuthenticationApp/views.py
@@ -12,22 +12,7 @@ def dashboard(request):
     return render(request, 'registration/Dashboard.html',{'section':'dashboard'})
 
 
-def sign_up(request):
-    if request.method == 'GET':
-        form = RegisterForm()
-        return render(request, 'register.html', {'form': form})    
-   
-    if request.method == 'POST':
-        form = RegisterForm(request.POST) 
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.username = user.username.lower()
-            user.save()
-            messages.success(request, 'You have singed up successfully.')
-            login(request, user)
-            return redirect('posts')
-        else:
-            return render(request, 'users/register.html', {'form': form})
+ 
 
 
 
